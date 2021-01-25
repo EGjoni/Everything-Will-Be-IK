282faef4408c39f5d3c4765dead4189ea75200e8
Eron <rufsketch1@gmail.com>
2021-01-25 02:19:28 -0500

    Improved orientation target weight predictability. Simplified limit cone generation code.

--- math/floatV/MathUtils.java
+++ math/floatV/MathUtils.java
@@ -383,4 +383,13 @@ public final class MathUtils {
 	public static float min(float a, float b) {
 		return a < b ? a : b;
 	}
+	
+	public static float invSqrt(float x) {
+	    float xhalf = 0.5f * x;
+	    int i = Float.floatToIntBits(x);
+	    i = 0x5f3759df - (i >> 1);
+	    x = Float.intBitsToFloat(i);
+	    x *= (1.5f - xhalf * x * x);
+	    return x;
+	}
 }
